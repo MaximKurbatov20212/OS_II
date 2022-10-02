2f3c289c1dcc93a59d03158cd0fe92e077b0aa50
maxwell <m.kurbatov@g.nsu.ru>
2022-10-02 19:39:30 +0700

    lab4, lab6 is already completed

--- lab5/main.c
+++ lab5/main.c
@@ -9,11 +9,20 @@
 #define SLEEP_ERROR -1
 #define CANCEL_SUCCESS 0
 #define TRUE 1
+#define NO_ARGS 0
+
+void print_cleanup_push_handler(void* args) {
+    printf("cleanup_push: %s\n" , (char *) args);
+}
 
 void* print_smth() {
+    pthread_cleanup_push(print_cleanup_push_handler, "Child thread");
+
     while (TRUE) {
         printf("New thread\n");
     }
+
+    pthread_cleanup_pop(NO_ARGS);
 }
 
 int main(int argc, char* argv[]) {
@@ -38,5 +47,5 @@ int main(int argc, char* argv[]) {
         return ERROR;
     }
 
-    return SUCCESS;
+    pthread_exit(NULL);
 }
